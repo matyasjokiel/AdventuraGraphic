7064a9cf7eb76d6fb8efecd52d7b6a4cef71b0ce
Matyas Jokiel <matyas.jokiel1@gmail.com>
2020-11-23 19:27:15 +0100

    Zmeny k lepsimu

--- src/main/java/cz/vse/jokiel/MainController.java
+++ src/main/java/cz/vse/jokiel/MainController.java
@@ -1,5 +1,6 @@
 package cz.vse.jokiel;
 
+import com.sun.deploy.panel.RuleSetViewerDialog;
 import cz.vse.jokiel.model.*;
 import javafx.scene.Cursor;
 import javafx.scene.control.Label;
@@ -28,8 +29,13 @@ public class MainController {
 
     public VBox exits;
     public VBox items;
+    public VBox itemsBox;
     public VBox npcs;
+    public VBox npcsBox;
     public VBox backpack;
+    public VBox backpackBox;
+    public VBox search;
+    public VBox searchBox;
 
 
     public void init(IGame game) {
@@ -45,9 +51,34 @@ public class MainController {
         String description = getCurrentArea().getFullDescription();
         locationDescription.setText(description);
 
+
+        npcsBox.setVisible(false);
+        backpackBox.setVisible(false);
+        itemsBox.setVisible(false);
+        searchBox.setVisible(false);
+
         updateExits();
         updateItems();
         updateNpcs();
+        updateBackpack();
+    }
+
+    private void updateBackpack() {
+        Set<Item> itemSet = game.getGamePlan().getBackpack().getItemSet();
+        backpack.getChildren().clear();
+
+        for(Item item : itemSet) {
+            String itemName = item.getName();
+            Label itemLabel = new Label(itemName);
+            itemLabel.setCursor(Cursor.HAND);
+
+            itemLabel.setOnMouseClicked(event -> {
+                executeCommand("odhod " + itemName);
+            });
+
+            backpack.getChildren().add(itemLabel);
+            backpackBox.setVisible(true);
+        }
     }
 
     private void updateNpcs() {
@@ -66,6 +97,7 @@ public class MainController {
                 });
 
                 npcs.getChildren().add(npcLabel);
+                npcsBox.setVisible(true);
             }
         }
     }
@@ -103,8 +135,10 @@ public class MainController {
     private void updateItems() {
         Set<Storage> storageList = getCurrentArea().getStorages();
         items.getChildren().clear();
+        search.getChildren().clear();
 
         for(Storage storage : storageList) {
+            //hlavni uloziste
             if(storage.getName().equals("Hlavni")) {
                 Set<Item> hlavniItems= storage.getItemSet();
                 for(Item item : hlavniItems) {
@@ -119,17 +153,41 @@ public class MainController {
                         });
                     }
                     items.getChildren().add(itemLabel);
+                    itemsBox.setVisible(true);
                 }
             } else {
+                // vedlejsi storages
+                if(storage.isAccessible()){
                 String storageName = storage.getName();
                 Label storageLabel = new Label(storageName);
 
                 storageLabel.setCursor(Cursor.HAND);
                 storageLabel.setOnMouseClicked(event -> {
-                    executeCommand("prohledej "+storageName);
+                    storage.setAccessibility(false);
+
+
+                    Set<Item> itemsInStorage = storage.getItemSet();
+
+                    for(Item item : itemsInStorage){
+                        String itemName = item.getName();
+                        Label itemLabel = new Label(itemName);
+
+                        if(item.isAccesible()) {
+                            itemLabel.setCursor(Cursor.HAND);
+
+                            itemLabel.setOnMouseClicked(event2 -> {
+                                executeCommand("vezmi "+itemName);
+                            });
+                        }
+                        getCurrentArea().removeItemFromAllStorages(item.getName());
+                        getCurrentArea().addItem(item);
+                        update();
+                    }
                 });
 
-                items.getChildren().add(storageLabel);
+                search.getChildren().add(storageLabel);
+                searchBox.setVisible(true);
+                }
             }
         }
 
